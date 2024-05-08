86e15114f52aba56254b0fbcb5ca6e8073ee4758
Dekline1 <mfigat@gmail.com>
2024-05-08 08:55:07 +0200

    id.txt to .gitignore, add adress of real UL database

--- html scraper.py
+++ html scraper.py
@@ -3,15 +3,12 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-scraper = pd.read_html("https://en.wikipedia.org/wiki/Printed_circuit_board")
+scraper = pd.read_html("https://iq.ul.com/pwb/Trade.aspx")
 
-for idx, table in enumerate(scraper):
-    print("*"*120)
-    print(idx)
-    print(table)
-    print("*" * 120)
+i = [0, 1,2,3]
+for df in scraper:
+    #print(df[i].to_string(header=False))
+    for line in df[i]:
+        line.to_csv('id.txt', header=False, index=False)
 
-print("*" * 120)
-print("*" * 120)
-print("*" * 120)
-print(scraper[3])
+
