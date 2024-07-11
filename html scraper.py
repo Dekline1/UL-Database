8bd4143bdf996c6323ef05c6d536ed399cb95692
Mateusz Figat <figat.m@assel.pl>
2024-07-11 11:59:30 +0200

    results saved as separated columns in *.xlsx file

--- html scraper.py
+++ html scraper.py
@@ -4,7 +4,8 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 url = "https://iq.ul.com/pwb/Trade.aspx"
 scraper = pd.read_html(url)
-print(scraper)
-scraper[0].to_csv("ULlist.csv", index=False)
-
+ULscrapSample = scraper[0]
+ULscrapFull = scraper[0]
+ULscrapSample.iloc[:20, [0, 1]].to_excel("ULlistSample.xlsx", index=False, header=False)
+ULscrapFull.iloc[:, [0, 1]].to_excel("ULlistFull.xlsx", index=False, header=False)
 # generuje csv bez rozdzielenia na nazwe producenta i jego UL
